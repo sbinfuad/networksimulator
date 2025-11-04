1ea33a802017ca9e878b36d156045ac96ad31ac4
Sanjid <sbinfuad@mun.ca>
2025-11-04 00:37:42 -0500

    Initial commit

---
+++ src/main/java/com/example/networksimulation/NetworksimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.networksimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworksimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworksimulationApplication.class, args);
+	}
+
+}
